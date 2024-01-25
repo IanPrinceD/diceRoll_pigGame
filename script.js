5cb2bc776ae5dcf9bd13235499bf314c9ffba3d4
IanPrinceD <princedumanhug@gmail.com>
2024-01-25 22:08:54 +0800

    added swicth player and score

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 'use strict';
 
 // naming variables
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
-const currentScore0el = document.getElementById('current--0')
+const currentScore0el = document.getElementById('current--0');
 const currentScore1el = document.getElementById('current--1');
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
@@ -15,7 +17,15 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
+
+// swicth effect (bg) of the player turn
+const toggle = function () {
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
 
 // rolling the dice
 btnRoll.addEventListener('click', function () {
@@ -30,9 +40,14 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     // add value of dice to current score
     currentScore += dice;
-    currentScore0el.textContent = currentScore
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
-    // swicth to next player
-    currentScore1el.textContent = currentScore;
+    // swicth to next player and reseting the score of the current player back to 0
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    // setting the current score back to 0
+    currentScore = 0;
+    toggle();
   }
 });
